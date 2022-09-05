9b85f16a7285e096f56a541f2df14f4ee2053957
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2022-09-05 14:37:59 +0200

    add js function for hide navbar and display navbar on scroll

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,20 @@
+// MENU HAMBURGER
+
 let toggle = document.querySelector('.toggle');
 let body = document.querySelector('body');
 
 toggle.addEventListener('click', function(){
     body.classList.toggle('open_menu');
-})
+});
+
+
+// DISPLAY NAVBAR ON SCROLL
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+}
